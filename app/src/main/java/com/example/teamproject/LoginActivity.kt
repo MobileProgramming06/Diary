2b7526c5c5dd529acb62c791cb06a8194dfbefd9
mdmmdm26 <minsh0826@gmail.com>
2024-05-29 20:02:39 +0900

    오류 해결

--- app/src/main/java/com/example/teamproject/LoginActivity.kt
+++ app/src/main/java/com/example/teamproject/LoginActivity.kt
@@ -1,6 +1,5 @@
 package com.example.teamproject
 
-import MainActivity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
--- app/src/main/java/com/example/teamproject/MainActivity.kt
+++ app/src/main/java/com/example/teamproject/MainActivity.kt
@@ -1,3 +1,5 @@
+package com.example.teamproject
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
